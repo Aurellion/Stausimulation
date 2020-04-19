f49a7a4a0e13b0b4fa4cab963b9623799df991ca
Johannes B <johannes.bock@live.de>
2020-04-20 00:49:35 +0200

    Projektdateien hinzufügen.

---
+++ Stausimulation/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Stausimulation
+{
+    class Car
+    {
+        //private double position;//Zahl der Runden
+        public double position { get; private set;}//2 Zahl der Runden
+
+        //private double speed;//Geschwindigkeit (Betrag) in Runden pro Minute
+
+        public double speed;//Geschwindigkeit (Betrag) in Runden pro Minute
+
+        private static Random random = new Random();
+        public Car()
+        {
+            position = random.NextDouble();
+            //speed = 10 * random.NextDouble();
+            speed = 8 + 4 * random.NextDouble();//2
+        }
+
+        public void Draw(Canvas zeichenflaeche)
+        {
+            Ellipse e = new Ellipse();
+            e.Width = 5;
+            e.Height = 5;
+            e.Fill = Brushes.Blue;
+            double angle = 2 * Math.PI * position;
+            Canvas.SetLeft(e, 300 + 150 * Math.Sin(angle));
+            Canvas.SetTop(e, 300 - 150 * Math.Cos(angle));
+            zeichenflaeche.Children.Add(e);
+        }
+
+        internal void Move(TimeSpan interval)
+        {
+            position += speed * interval.TotalMinutes;
+        }
+    }
+}
---
+++ Stausimulation/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stausimulation
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        Car[] autos = new Car[20];
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Start();
+            timer.Tick += animate;
+
+
+            for (int i = 0; i < autos.Length; i++)
+            {
+                autos[i] = new Car();
+            }
+        }
+
+        void animate(object sender, EventArgs e)
+        {
+            Zeichenflaeche.Children.Clear();
+            //Autos durchgehen und zeichnen
+            foreach (Car auto in autos)
+            {
+                //2 den naehsten Vorgaenger finden
+                double minDistance = 42.0;// nicht 1.0 zur Sicherheit
+                Car naehstesAuto=null;
+                foreach (Car anderesauto in autos)
+                {
+                    if (anderesauto == auto) continue;
+                    //     -0.9     =      4.2             -    5.1    
+                    double distance = anderesauto.position - auto.position;
+                    //     0.1      = 0.9-(-1)
+                    distance -= Math.Floor(distance);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        naehstesAuto = anderesauto;
+                    }
+                }
+                //2 Geschwindigkeit reduzieren
+                if (minDistance < 0.02 && auto.speed > naehstesAuto.speed)
+                {
+                    auto.speed = naehstesAuto.speed;
+                }
+
+
+                auto.Move(timer.Interval);
+            }
+
+            foreach (Car auto in autos)
+            {
+                auto.Draw(Zeichenflaeche);
+            }
+        }
+    }
+}
